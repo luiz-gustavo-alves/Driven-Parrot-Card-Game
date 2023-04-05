dc6490f85eeb7ba0d62b0ec33284ceb4d03a4532
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-04-04 21:30:00 -0300

    Added selectCards + shuffle function
    Created global array to store a collection of cards that will be used in the game.
    
    Functions details:
    
    selectCards(): Selects all the cards that will be used in the game, cards will be stored in global array "cardsList".
    shuffle(): Pseudorandom shuffle cards.

--- scripts/general.js
+++ scripts/general.js
@@ -1,5 +1,12 @@
-/* Variável global que armazena a quantidade total de cartas por jogo */
+/* Variável global que armazena a quantidade total de cartas do jogo */
 let numCards;
+
+/* Array global que armazena o conteudo das cartas do jogo */
+let cardsList = [];
+
+const cardContainer = document.querySelectorAll(".cards-container");
+const maxNumberCardsContainer = cardContainer[0].children.length;
+console.log(maxNumberCardsContainer);
 
 startGame();
 
@@ -14,36 +21,63 @@ function startGame() {
         if (!Number.isInteger(numCards)) {
             continue;
         }
-    
-        /*  Verifica se o numero de cartas é impar ou se a quantidade de 
-         *  cartas é menor que 4 ou maior que 14  
-         */
+     
+         /*  Verifica se o numero de cartas é impar ou se a quantidade de 
+          *  cartas é menor que 4 ou maior que 14  */
         if (numCards % 2 == 1 || (numCards < 4 || numCards > 14)) {
             continue;
         }
     
         gameStart = true;
     }
-    showCards();
+    selectCards();
+}
+
+function selectCards() {
+ 
+    /* Função seleciona e armazena as cartas que serão utilizadas no jogo */
+
+    let counter = 0;
+    for (let i = 0; i < cardContainer.length; i++) {
+
+        let cards = cardContainer[i].querySelectorAll(".card");
+        
+        for (let j = 0; j < cards.length; j++) {
+
+            cardsList.push(cards[j]);
+            counter++;
+
+            cardContainer[i].removeChild(cardContainer[i].children[0]);
+
+            if (counter >= numCards) {
+
+                cardsList.sort(shuffle);
+                showCards();
+                return;
+            }
+        }
+    }
+}
+
+function shuffle() {
+
+    /*  Função embaralha as cartas de forma pseudo-aleatória */
+    return Math.random() - 0.5;
 }
 
 function showCards() {
 
-    const main = document.querySelector(".main");
-
-    /* Numero de cartas máxima para cada cardContainer */
-    const maxNumCards = 6;
+    /* Função remove a classe hidden e mostra as cartas na tela do usuário */
 
     let counter = 0;
-    for (let i = 0; i < main.children.length; i++) {
-
-        let cardContainer = main.children[i];
+    for (let i = 0; i < cardContainer.length; i++) {
         
-        for (let j = 0; j < maxNumCards; j++) {
+        for (let j = 0; j < maxNumberCardsContainer; j++) {
 
-            cardContainer.children[j].classList.remove("hidden");
+            cardsList[counter].classList.remove("hidden");
+            cardContainer[i].appendChild(cardsList[counter]);
             counter++;
-
+            
             if (counter >= numCards) {
                 return;
             }
